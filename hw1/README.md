594a82ecc427807dbdd2353ea12cd3275659d823
topjohnwu <topjohnwu@gmail.com>
2017-10-28 13:40:21 +0800

    Update to Github links

---
+++ hw1/README.md
@@ -0,0 +1,4 @@
+# HW1
+
+My scripts already contains a token to use `curl` to download the file from Gthub, but in case that the model download fails, you can download the file `models.tgz` with URL [https://github.com/topjohnwu/ADLxMLDS2017/releases/download/hw1/models.tgz](https://github.com/topjohnwu/ADLxMLDS2017/releases/download/hw1/models.tgz) (note: this URL only works in browsers that is authenticated).
+
--- hw1/hw1_rnn.sh
+++ hw1/hw1_rnn.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 if [ ! -d models ]; then
 	mkdir models
-	curl -L https://www.dropbox.com/s/1ojzxc41dmiinhk/models.tgz?dl=1 | tar zxf - -C models
+	curl -L -H \
+	'Accept: application/octet-stream' \
+	'https://8f721a61667c6b2052b76170bca4b0755cd45ad3:@api.github.com/repos/topjohnwu/ADLxMLDS2017/releases/assets/5187687' \
+	| tar zxf - -C models
 fi
 python run.py test rnn 500 "$1" rnn.ckpt "$2"
