88fbd6fcf0d38e881526676d29d5459e630fecae
topjohnwu <topjohnwu@gmail.com>
2017-11-19 22:42:42 +0800

    Rename script

--- hw2/hw2_seq2seq.sh
+++ hw2/hw2_seq2seq.sh
@@ -6,5 +6,5 @@ if [ ! -d models/vanilla ]; then
 	'https://8f721a61667c6b2052b76170bca4b0755cd45ad3:@api.github.com/repos/topjohnwu/ADLxMLDS2017/releases/assets/5390031' \
 	| tar zxf - -C models
 fi
-python run.py test vanilla "$1" "$2"
-python run.py peer vanilla "$1" "$3"
+python model_seq2seq.py test vanilla "$1" "$2"
+python model_seq2seq.py peer vanilla "$1" "$3"
--- hw2/hw2_special.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python run.py special "$1" "$2"
